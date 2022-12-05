0d4170f1f50049dbf522c9f8e730a4e804b8c0d1
Emily <emilldr>
2022-12-05 12:58:42 -0600

    init

---
+++ src/main/java/net/yorksolutions/emilymilldrumblogcmscapstonebe/EmilyMilldrumBlogCmsCapstoneBeApplication.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.emilymilldrumblogcmscapstonebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmilyMilldrumBlogCmsCapstoneBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmilyMilldrumBlogCmsCapstoneBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/yorksolutions/emilymilldrumblogcmscapstonebe/EmilyMilldrumBlogCmsCapstoneBeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.emilymilldrumblogcmscapstonebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmilyMilldrumBlogCmsCapstoneBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
